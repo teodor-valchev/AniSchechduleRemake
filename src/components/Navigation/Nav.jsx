432d59c033693a403f8de019bb8f5e77a01cf8f3
Teodor Valchev <teo.thenx@gmail.com>
2024-10-29 11:25:28 +0200

    moved the navigation to a separate component

---
+++ src/components/Navigation/Nav.jsx
@@ -0,0 +1,128 @@
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+} from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+    { name: "AnimeSchedule.net", href: "#", current: false },
+    { name: "Anime", href: "#", current: true },
+    { name: "Seasons", href: "#", current: false },
+    { name: "Upcoming", href: "#", current: false },
+];
+const userNavigation = [
+    { name: "Your Profile", href: "#" },
+    { name: "Settings", href: "#" },
+    { name: "Sign out", href: "#" },
+];
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+}
+/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-100">
+        <body class="h-full">
+        ```
+      */
+
+const Navigation = () => {
+    return (
+        <Disclosure as="nav" className="bg-gray-800">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-16 items-center justify-between">
+                    <div className="flex items-center">
+                        <div className="flex-shrink-0">
+                            <img
+                                alt="Your Company"
+                                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
+                                className="h-8 w-8"
+                            />
+                        </div>
+                        <div className="hidden md:block">
+                            <div className="ml-10 flex items-baseline space-x-4">
+                                {navigation.map((item) => (
+                                    <a
+                                        key={item.name}
+                                        href={item.href}
+                                        aria-current={
+                                            item.current ? "page" : undefined
+                                        }
+                                        className={classNames(
+                                            item.current
+                                                ? "bg-gray-900 text-white"
+                                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                            "rounded-md px-3 py-2 text-sm font-medium"
+                                        )}
+                                    >
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="hidden md:block">
+                        <div className="ml-4 flex items-center md:ml-6">
+                            {/* Login and sign up logic */}
+                        </div>
+                    </div>
+                    <div className="-mr-2 flex md:hidden">
+                        {/* Mobile menu button */}
+                        <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                            <span className="absolute -inset-0.5" />
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon
+                                aria-hidden="true"
+                                className="block h-6 w-6 group-data-[open]:hidden"
+                            />
+                            <XMarkIcon
+                                aria-hidden="true"
+                                className="hidden h-6 w-6 group-data-[open]:block"
+                            />
+                        </DisclosureButton>
+                    </div>
+                </div>
+            </div>
+
+            <DisclosurePanel className="md:hidden">
+                <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                    {navigation.map((item) => (
+                        <DisclosureButton
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            aria-current={item.current ? "page" : undefined}
+                            className={classNames(
+                                item.current
+                                    ? "bg-gray-900 text-white"
+                                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                "block rounded-md px-3 py-2 text-base font-medium"
+                            )}
+                        >
+                            {item.name}
+                        </DisclosureButton>
+                    ))}
+                </div>
+                <div className="border-t border-gray-700 pb-3 pt-4">
+                    <div className="mt-3 space-y-1 px-2">
+                        {userNavigation.map((item) => (
+                            <DisclosureButton
+                                key={item.name}
+                                as="a"
+                                href={item.href}
+                                className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                            >
+                                {item.name}
+                            </DisclosureButton>
+                        ))}
+                    </div>
+                </div>
+            </DisclosurePanel>
+        </Disclosure>
+    );
+};
+
+export default Navigation;
