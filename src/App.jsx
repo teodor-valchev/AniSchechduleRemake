30ce2fafd87249a80d85d72f68b4baf21f2d975e
Teodor Valchev <teo.thenx@gmail.com>
2024-10-29 11:02:06 +0200

    Changed facvicon Changed website navigation and renamed links

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,9 @@ const user = {
         "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-    { name: "Dashboard", href: "#", current: true },
-    { name: "Team", href: "#", current: false },
-    { name: "Projects", href: "#", current: false },
-    { name: "Calendar", href: "#", current: false },
-    { name: "Reports", href: "#", current: false },
+    { name: "Anime", href: "#", current: true },
+    { name: "Seasons", href: "#", current: false },
+    { name: "Upcoming", href: "#", current: false },
 ];
 const userNavigation = [
     { name: "Your Profile", href: "#" },
@@ -221,7 +219,7 @@ const App = () => {
                 <header className="bg-white shadow">
                     <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                         <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-                            Dashboard
+                            AniScheduleRemake
                         </h1>
                     </div>
                 </header>
