f753e47771bfdd4d985906ceccae6638b6e0aced
Teodor Valchev <teo.thenx@gmail.com>
2024-11-11 17:20:13 +0200

    added some styling to the cards

--- src/components/Home/AnimeItem.jsx
+++ src/components/Home/AnimeItem.jsx
@@ -1,14 +1,41 @@
 
 
-const AnimeItem = ({ title, imageVersionRoute }) => {
-    console.log(imageVersionRoute);
+const AnimeItem = ({ title, imageVersionRoute, dubPremier, episodes, genres }) => {
+    const utcDate = new Date(dubPremier);
+
+    // Convert to your local time (Bulgaria timezone)
+    const localDate = utcDate.toLocaleString("en-BG", {
+        timeZone: "Europe/Sofia",
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+    });
 
     return (
-        <div key={""} className="group relative">
+        <div className="group relative">
+            <div className="text-2xl font-bold tracking-tight text-gray-900 text-center">
+                {title}
+            </div>
+            <div>
+                <div className="info flex items-center justify-evenly">
+                    <span>Dub premiere: {localDate}</span>
+                    <span>Episodes: {episodes ? episodes : "UNKNOWN"}</span>
+                </div>
+                <div className="genres flex justify-between bg-antiquewhite">
+                    {genres.map((g) => (
+                        <span className="genre">
+                            <span>{g.name}</span>
+                        </span>
+                    ))}
+                </div>
+            </div>
             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                 <img
                     alt={title}
-                    src={`https://img.animeschedule.net/production/assets/public/img/` + imageVersionRoute}
+                    src={
+                        `https://img.animeschedule.net/production/assets/public/img/` +
+                        imageVersionRoute
+                    }
                     className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
             </div>
