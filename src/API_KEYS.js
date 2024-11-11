4b1ea778b1a8a748008a15ea60b15bcdbe7a887c
Teodor Valchev <teo.thenx@gmail.com>
2024-11-11 15:23:39 +0200

    remove API_KEYS

--- src/API_KEYS.js
+++ src/API_KEYS.js
@@ -1,1 +1,4 @@
 
+export const ANIME_SCHEDULE_URL =
+    "https://cors-anywhere.herokuapp.com/https://animeschedule.net/api/v3/anime";
+export const Anime_Schedule_Token = "oJ5fPZXWDU2z2vewknGx2zpNYZEOw";
